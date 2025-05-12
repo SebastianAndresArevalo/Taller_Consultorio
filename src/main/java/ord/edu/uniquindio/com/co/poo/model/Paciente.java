f88cddb63e7fa32e3f2fa989b9547da20ddc787d
SebastianAndresArevalo <sebastiana.arevalop@uqvirtual.edu.co>
2025-05-11 20:04:34 -0500

    Update Paciente.java

--- src/main/java/ord/edu/uniquindio/com/co/poo/model/Paciente.java
+++ src/main/java/ord/edu/uniquindio/com/co/poo/model/Paciente.java
@@ -1,4 +1,12 @@
 package ord.edu.uniquindio.com.co.poo.model;
 
-public class Paciente {
+import java.time.LocalDate;
+
+public record Paciente(String historiaClinica, String nombre, int edad, String telefono, String direccion, LocalDate fechaUltimaConsulta, int tratamientosRealizados) {
+
+    public Paciente actualizarRegistro(LocalDate nuevaFecha, int nuevosTratamientos) {
+
+        return new Paciente(this.historiaClinica, this.nombre, this.edad, this.telefono, this.direccion, nuevaFecha, nuevosTratamientos);
+
+    }
 }
